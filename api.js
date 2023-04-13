762c5707a839cecc08dce4f14faa6085c9e5cae6
xenonv1 <82435371+xenonv1@users.noreply.github.com>
2023-04-13 16:18:04 +0200

    added service.json file and implemented service endpoints

--- api.js
+++ api.js
@@ -1,14 +1,41 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 
 const ipAddr = "127.0.0.1";
 const port = 8000;
 
+const services = JSON.parse(fs.readFileSync("./services.json", "utf-8"));
+
 /* - - - server - - - */
 
-app.listen(ipAddr, port, () => {
+app.listen(port, ipAddr, () => {
   console.log(`Now listening on ${ipAddr}:${port}...`);
 });
 
 /* - - - endpoints - - - */
+
+app.get("/services", (req, res) => {
+  res.set("Content-Type", "application/json");
+  res.end(JSON.stringify(services));
+});
+
+app.get("/service/:pid", (req, res) => {
+  res.set("Content-Type", "application/json");
+
+  const pid = req.params.pid;
+
+  let service;
+  services.forEach((item) => {
+    if (item.id == pid) service = item;
+  });
+
+  if (service) {
+    res.end(JSON.stringify(service));
+  } else {
+    res.end(`service with id ${pid} not found.`);
+  }
+});
+
+/* - - - functions - - - */
