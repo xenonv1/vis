266746d3ce5efda465e937e1436b66498818b7dd
Maximilian Jung <maximilian.jung1997@gmail.com>
2023-04-16 17:33:21 +0200

    add .gitignore

--- api.js
+++ api.js
@@ -1,26 +1,32 @@
 const express = require('express');
-const fs = require('fs');
-
 const app = express();
+const fs = require('fs');
 
 const ipAddr = "127.0.0.1";
 const port = 8000;
 
 /* - - - server - - - */
 
-app.listen(port,ipAddr, () => {
+app.listen(port, ipAddr, () => {
     console.log(`Now listening on ${ipAddr}:${port}...`);
 });
 
 /* - - - endpoints - - - */
 
-app.get('/products', (req, res) => {
-    fs.readFile('products.json', 'utf-8', (err, data) => {
-        if(err) {
+app.get('/products/:id', (req, res) => {
+    const id = req.params.id;
+    fs.readFile('products.json', 'utf8', (err, data) => {
+        if (err) {
             console.error(err);
-            return req.statusCode(500), send('Error reading Products');
+            res.status(500).send('Error reading file');
+            return;
         }
         const products = JSON.parse(data);
-        res.json(products);
+        const product = products.find(p => p.id === id);
+        if (!product) {
+            res.status(404).send('Product not found');
+            return;
+        }
+        res.json(product);
     });
-  });
+});
