e957060b8997bd2c8774559ff90bcafb12b675ca
Maximilian Jung <maximilian.jung1997@gmail.com>
2023-04-16 18:50:50 +0200

    added product id search to api.js adds package-lock.json & package.json

--- api.js
+++ api.js
@@ -5,28 +5,33 @@ const fs = require('fs');
 const ipAddr = "127.0.0.1";
 const port = 8000;
 
+let products;
+
 /* - - - server - - - */
 
-app.listen(port, ipAddr, () => {
-    console.log(`Now listening on ${ipAddr}:${port}...`);
+fs.readFile('products.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    products = JSON.parse(data);
+    app.listen(port, ipAddr, () => {
+        console.log(`Now listening on ${ipAddr}:${port}...`);
+    });
 });
 
 /* - - - endpoints - - - */
 
+app.get('/products', (req, res) => {
+    res.json(products);
+});
+
 app.get('/products/:id', (req, res) => {
     const id = req.params.id;
-    fs.readFile('products.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Error reading file');
-            return;
-        }
-        const products = JSON.parse(data);
-        const product = products.find(p => p.id === id);
-        if (!product) {
-            res.status(404).send('Product not found');
-            return;
-        }
-        res.json(product);
-    });
+    const product = products.find(p => p.id === parseInt(id));
+    if (!product) {
+        res.status(404).send('Product not found');
+        return;
+    }
+    res.json(product);
 });
